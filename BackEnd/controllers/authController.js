c08fed2eecdb9f83b695d08edeb9cc72b7cb76eb
pradeep sakthi 👋 <spradeepsakthi2004@gmail.com>
2025-02-01 21:24:15 +0530

    update:add bcryptjs encoding to password

--- BackEnd/controllers/authController.js
+++ BackEnd/controllers/authController.js
@@ -1,16 +1,20 @@
 const userModel = require('../models/userModel')
 const { generateToken } = require('../utils/jwtUtils')
+const bcrypt=require('bcryptjs')
 const login = (req, res) => {
     const { username, password } = req.body;
     userModel.findOne({ username: username }).then((result) => {
-        console.log('result')
-        if (result.password === password) {
+        if (bcrypt.compare(password,result.password)) {
             const payload = {
                 userId: result._id,
                 username: username,
-
             }
             const token = generateToken(payload);
+            res.cookie('jwt',token,{
+                maxAge:60*60*1000,
+                httpOnly:true,
+                sameSite:"strict",
+            })
             res.status(200).json({ success: true, token: token });
         }
         else {
@@ -23,11 +27,14 @@ const login = (req, res) => {
 
 }
 
-const register = (req, res) => {
-    userModel.create(req.body).then(() => {
-        res.status(201).json({ success: true })
+const register = async(req, res) => {
+    const {username,name,password}=req.body;
+    const hashedPassword=await bcrypt.hash(password,10)
+    userModel.create({username:username, name:name, password:hashedPassword}).then((result) => {
+        res.status(201).json({ success: true ,message:result})
     }).catch((error) => {
-        res.status(409).json({ code: error.errorResponse.code })
+        console.log(error)
+        res.status(409).json(error)
     })
 }
 
