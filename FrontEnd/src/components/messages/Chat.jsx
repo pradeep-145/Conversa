8a4bb1031e1967b2efd7b3eaed868072557839f2
pradeep sakthi 👋 <spradeepsakthi2004@gmail.com>
2025-02-10 21:59:00 +0530

    feat: Implement chat functionality with message input and display components

--- FrontEnd/src/components/messages/Chat.jsx
+++ FrontEnd/src/components/messages/Chat.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import Messages from './Messages'
+import MessageInput from './MessageInput'
 const Chat = () => {
+  const noChatSelected=true
   return (
-    <div>Chat</div>
+    <div className='md:min-w-[450px] flex flex-col'>
+      {noChatSelected?<NoChatSelected></NoChatSelected>:
+      <>
+      <div className='bg-slate-500 px-4 py-2 mb-2'>
+        <span className='label-text '>To:</span>
+        <span className='text-gray-900 font-bold'>JohnDoe</span>
+      </div>
+
+       <Messages></Messages>
+       <MessageInput></MessageInput>
+      </>}
+    </div>
   )
 }
 
 export default Chat
+
+const NoChatSelected=()=>{
+  return(
+    <div className='flex-1 flex items-center justify-center'>
+      <p className='text-gray-200 text-xl'>Select a chat to start messaging</p>
+    </div>
+  )
+}
--- FrontEnd/src/components/sidebar/Conversation.jsx
+++ FrontEnd/src/components/sidebar/Conversation.jsx
@@ -5,7 +5,7 @@ const Conversation = ({ conversation }) => {
         <div className='flex items-center gap-2 rounded p-2 py-1 hover:bg-pink-500 cursor-pointer'>
             <div className='avator online'>
                 <div className='w-12 rounded-full '>
-                    <img src="" alt="user avatar" />
+                    <img src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png" alt="user avatar" />
                 </div>
             </div>
 
