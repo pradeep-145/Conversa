96c2649957e3e6d7cae2aa6ad9ba2665167a44a7
pradeep sakthi 👋 <spradeepsakthi2004@gmail.com>
2025-02-13 12:00:21 +0530

    feat: Add redirect for unauthenticated users to sign-in page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       <div className="h-screen p-4 flex items-center justify-center">
         <BrowserRouter>
           <Routes>
+            <Route path="/" element={authUser ? <Navigate to="/chat" /> : <Navigate to="/sign-in" /> } />
             <Route path="/sign-up" element={authUser ? <Navigate to="/chat" /> : <SignUp />} />
             <Route path="/sign-in" element={authUser ? <Navigate to="/chat" /> : <SignIn />} />
             <Route path="/chat" element={authUser ? <Home /> : <Navigate to="/sign-in" />} />
